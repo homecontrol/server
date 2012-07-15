c92d24afb86f9331246cac2ccb850ce30fa4c5a1
Daniel Morlock <daniel.morlock@gmail.com>
2012-07-15 22:00:54 +0200

    Ignore compiled python files for GIT.

--- src/homecontrol/handler.py
+++ src/homecontrol/handler.py
@@ -1,160 +1,64 @@
 import logging as log, os
 from BaseHTTPServer import BaseHTTPRequestHandler
 from genshi.template.base import TemplateSyntaxError
-from genshi.template.loader import TemplateNotFound
 from macpath import realpath
 
 class HCHandler(BaseHTTPRequestHandler):
     
-    plugin = None
-    
-    def send_response(self, code, message=None):
-        
-        if code != 200:
-            if self.plugin is not None:
-                log.warn("[%s]: %s" % (self.plugin.name, message))
-            else: log.warn(message)
-            
-        return BaseHTTPRequestHandler.send_response(self, code, message)
-    
-    def get_plugin_name(self):
-        
-        if self.path == None or self.path == "/":
-            return "index" # Default plugin
-
-        return self.path[1:].split("/")[0]
-    
-    def get_plugin_path(self):
-        """ Returns relative plugin path.
-        
-        The plugin path is relativ to the appropriate plugin. 
-        Example: For the path /index/foo/bar, the plugin is "index" and the 
-        plugin path will be "/foo/bar".
-        
-        Returns:
-            The appropriate plugin path.
-        """
-        
-        token = self.path[1:].split("/")
-        return "/" + "/".join(token[1:])
-    
-    def invoke_plugin_method(self):
-        """ Invokes plugin method.
-        
-        Tries to determine an appropriate plugin method to invoke from the current
-        request path.
-        
-        Example: 
-            For the path /index/foo/bar, the plugin is "index", the method is 
-            "foo" and "bar" is the first argument.
-        
-        Returns:
-            False if the no method could be determined from the current request
-            path or the given plugin does not have such a method. Returns true 
-            if the method could be called successfully. 
-        """
-        
-        if self.plugin == None:
-            return False
-        
-        token = self.path[1:].split("/")
-        if len(token) < 2: return False
-
-        plugin_name = token[0]        
-        method_name = token[1]
-        args = token[2:]
-        
-        if not hasattr(self.plugin, method_name):
-            return False
-
-        method = getattr(self.plugin, method_name)
-        if not callable(method):
-            return False
-        
-        method(self, *args)
-        return True
-    
-    def get_content_type(self):
-        
-        if self.path.endswith("png"): 
-            return "image/png"
-        
-        if self.path.endswith("gif"): 
-            return "image/gif"
-        
-        if self.path.endswith("css"): 
-            return "text/css"
-        
-        if self.path.endswith("js"): 
-            return "application/javascript"
-        
-        return None
-    
-    def get_abs_path(self):
-        """ Returns absolute path.
-        
-        Prepends the document root to the the absolute path. Further it is 
-        checked whether the real path is still within the document root for security 
-        issues.
-        
-        Returns:
-            None if the absolute path is outside the document root in this case
-            a 403 permission denied error will be replied. Otherwise the absolute
-            filesystem path will be returned.
-        """        
-        
-        path = os.path.realpath(self.server.document_root + os.sep + 
-                                "plugins" + os.sep + self.path)
-        
-        if path[:len(self.server.document_root)] != self.server.document_root:
-            self.send_response(403, "Permission denied for resource \"%s\"" % path)
-            return None
-        
-        return path
-    
     def do_GET(self):
         
-        content_type = self.get_content_type()
-        abs_path = self.get_abs_path()
+        token = None
+        plugin_name = None
         
-        if content_type != None:
+        if token != "/":
+            token = self.path.split("/")
 
-            try:
+        if token == None or len(token[0]) == 0:
+            plugin_name = "index" # Default plugin.
+        else: plugin_name = token[0]
+        
+        type = None;
+        if self.path.endswith("png"): type = "image/png"
+        elif self.path.endswith("gif"): type = "image/gif"
+        elif self.path.endswith("css"): type = "text/css"
+        elif self.path.endswith("js"): type = "text/js"
+
+        if type != None:
+            
+            src_path = self.server.document_root + os.sep + "plugins" + os.sep + plugin_name + os.sep + self.path
+
+            if not os.path.exists(src_path):
+                src_path = self.server.document_root + os.sep + self.path
                 
-                fd = open(abs_path, "rb")
+            src_path = os.path.realpath(src_path)
+            
+            if src_path[:len(self.server.document_root)] != self.server.document_root:
+                log.warn("Skipped loading resource not in document root \"%s\"" % src_path)
+                return
+            
+            try:
+                fd = open(src_path, "rb")
                 self.send_response(200) 
-                self.send_header("Content-type", content_type)
+                self.send_header("Content-type", type)
                 self.end_headers()
                 self.wfile.write(fd.read())
                 fd.close()
                 
             except IOError, e:
-
-                self.send_response(404, "File \"%s\" not found." % abs_path) 
+                log.error(e)
             
             return
-        
-        plugin_name = self.get_plugin_name()
-        plugin_path = self.get_plugin_path()
-        
+                
         if plugin_name not in self.server.plugins:
-            self.send_response(404, "Plugin \"%s\" not found." % plugin_name)
+            self.send_response(400, "Plugin \"%s\" not found." % plugin_name)
             return
             
-        self.plugin = self.server.plugins[plugin_name]        
-
-        try:
-            
-            if self.invoke_plugin_method() == True:
-                return
-
-            if self.plugin.handle_get(self, plugin_path) == True:
-                return
-            
-            self.send_response(404, "No handler found for path \"%s\"" % plugin_path)
+        plugin = self.server.plugins[plugin_name]    
+        
+        try:        
+            plugin.handle_get(self, "/".join(token[1:]))
             
         except TemplateSyntaxError, e:
-            self.plugin.log_error("Template error: %s" % e)
+            log.error("Error in handling GET request for plugin \"%s\": %s" %
+                (plugin_name, e))
             
-        except TemplateNotFound, e:
-            self.plugin.log_error("Template error: %s" % e)
--- src/homecontrol/plugin.py
+++ src/homecontrol/plugin.py
@@ -1,6 +1,10 @@
+import os
 import logging as log
+from exceptions import NotImplementedError
+from genshi.template import TemplateLoader, MarkupTemplate, Context
+from genshi.input import HTML
 
-class HCPlugin(object):
+class HCPlugin:
     
     server = None
     name = None
@@ -8,19 +12,61 @@ class HCPlugin(object):
     
     def __init__(self, server):
         self.server = server
-        self.name = self.__module__
+        
+        template_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep + \
+            "plugins" + os.sep + self.__module__
+
+        self.log_debug("Using templates from \"%s\"" % template_dir)
+        self.template = TemplateLoader(template_dir)
         
     def handle_get(self, handler, path):
-        return False
+        raise NotImplementedError
     
     def log_warn(self, msg):
-        log.warn("[%s]: %s" % (self.name, msg))
+        log.warn("[%s]: %s" % (self.__module__, msg))
         
     def log_debug(self, msg):
-        log.debug("[%s]: %s" % (self.name, msg))
+        log.debug("[%s]: %s" % (self.__module__, msg))
         
     def log_info(self, msg):
-        log.info("[%s]: %s" % (self.name, msg))
+        log.info("[%s]: %s" % (self.__module__, msg))
         
-    def log_error(self, msg):
-        log.error("[%s]: %s" % (self.name, msg))        
+    def send_html_response(self, handler, html_file, code = 200, skip_template = False, **kwargs):
+        """ Generates and sends an HTML response.
+
+        This generates headers and an HTML response either from the specified HTML 
+        source or HTML file. Both will be parsed using the Genhsi template engine
+        and will be extended with the default template.
+
+        Args:
+            handler: References the handler of the current http request.
+            code: Defines the response code is send within the http headers, 
+                by default, responde code 200 (success) is sent.
+            html_file: Must reference a HTML document within the current 
+                document root or the plugin directory that will be loaded and
+                parsed using Genshi.
+            **kwargs: Any additional parameter will be forwarded to the Genshi 
+                template.
+        """        
+        
+        handler.send_response(code)
+        handler.send_header("Content-type", 'text/html')
+        handler.end_headers()
+            
+        if skip_template:
+
+            handler.wfile.write(
+                self.template.load(kwargs["html"]).generate(**kwargs).render())
+
+        else:
+
+            template_path = os.path.dirname(os.path.realpath(__file__)) + os.sep + \
+                "assets" + os.sep + "html" + os.sep + "index.html"
+            fd = open(template_path)
+            template = MarkupTemplate(fd, template_path)
+            fd.close()
+        
+            html = HTML(self.template.load(html_file).generate(**kwargs).render())
+            template = template.generate(Context(input=html))
+        
+            handler.wfile.write(template.render())
