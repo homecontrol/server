f81692902b0ac1e6536de1e691285f565c662c98
Daniel Morlock <daniel.morlock@gmail.com>
2012-09-20 10:26:51 +0200

    Extended signals pane with event types and signal edit dialog.

--- src/homecontrol/plugins/scheduler/assets/js/scheduler.js
+++ src/homecontrol/plugins/scheduler/assets/js/scheduler.js
@@ -39,21 +39,52 @@
 			{
 				var $tbody = $("table tbody", this.$signals);
 				$("tr:not(.template)", $tbody).remove();
-				$.each(signals, function(i, signal)
+				$.each(signals, $.proxy(function(i, signal)
 				{
 					var $row = $("table tr.template", this.$signals).clone();
 					$row.removeClass("template").appendTo($tbody);
 					
-					signal["num_events"] = signal["events"].length;
+					signal["num_events"] = signal["events"].length;					
+					signal["event_types"] = $(document.createElement("div"));					
+					
+					var event_types = new Array();
+					for(var i in signal["events"])
+					{
+						var event = signal["events"][i];
+						if($.inArray(event.type, event_types) < 0)
+							event_types.push(event.type);
+					}
+					event_types.sort();					
+					for(var i in event_types)
+					{
+						var $el = $(document.createElement("span"));
+						$el.html(event_types[i]);
+						$el.addClass("label");
+						
+						switch(event_types[i])
+						{
+							case "ir":
+								$el.addClass("label-important");
+							break;
+							case "rf":
+								$el.addClass("label-warning");
+							break;							
+						}
+						
+						$el.appendTo(signal["event_types"]);
+					}
+					
+					signal["event_types"] = signal["event_types"].html();
+					
 					for(var key in signal)
 					{
 						$row.html($row.html().replace("#" + key, signal[key]));
 					}
 					
 					// Add signal id for further editing.
-					$row.data("id", signal["id"]);
-					
-				});
+					$row.data("id", signal["id"]);	
+					this.set_signal_handlers($row, signal);
+				}, this));
 				
 				this.loader_signals.hide();
 				$("table", this.$signals).fadeIn();				
@@ -79,6 +110,26 @@
 
 			this.loader_scheduler.hide();
 			$("table", this.$scheduler).fadeIn();			
+		},
+		
+		set_signal_handlers: function($row, signal)
+		{
+			$dialog = $(".templates .dialog-edit-signal").clone().appendTo($row);
+			$dialog.dialog(
+			{
+				title: $dialog.data("title"),
+				modal: true,
+				resizable: false,
+				width: $dialog.outerWidth(true) + "px",
+				autoOpen: false
+			});
+			
+			$row.click($.proxy(function()
+			{
+				$dialog.dialog("open");
+				console.log(signal);
+				
+			}, this));
 		}
 	};
 	
