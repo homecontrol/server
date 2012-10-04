92874ce4d6b12e6b9eaa44388e82ff3592369fcc
Daniel Morlock <daniel.morlock@gmail.com>
2012-10-04 22:58:28 +0200

    Introduced signal view class, moved edit/delete dialogs from scheduler to the new signal view.

--- src/homecontrol/plugins/scheduler/assets/js/scheduler.js
+++ src/homecontrol/plugins/scheduler/assets/js/scheduler.js
@@ -44,17 +44,11 @@
 					var $row = $("table tr.template", this.$signals).clone();
 					$row.removeClass("template").appendTo($tbody);
 					
-					signal["num_events"] = signal["events"].length;					
+					var event_types = signal["event_types"]
+					signal["num_events"] = signal["events"].length;
 					signal["event_types"] = $(document.createElement("div"));					
-					
-					var event_types = new Array();
-					for(var i in signal["events"])
-					{
-						var event = signal["events"][i];
-						if($.inArray(event.type, event_types) < 0)
-							event_types.push(event.type);
-					}
-					event_types.sort();					
+
+					event_types.sort();
 					for(var i in event_types)
 					{
 						var $el = $(document.createElement("span"));
@@ -83,7 +77,7 @@
 					
 					// Add signal id for further editing.
 					$row.data("id", signal["id"]);	
-					this.set_signal_handlers($row, signal);
+					$row.click(function(){location.href = "/signal/view?signal_id=" + signal["id"];})
 				}, this));
 				
 				this.loader_signals.hide();
@@ -110,123 +104,6 @@
 
 			this.loader_scheduler.hide();
 			$("table", this.$scheduler).fadeIn();			
-		},
-		
-		set_signal_handlers: function($row, signal)
-		{
-			var $dialog = $(".templates .dialog-edit-signal").clone().appendTo($("td:first", $row));
-			var $confirm = $(".templates .dialog-delete-signal-confirm").clone().appendTo($("td:first", $row));
-			var $input_name = $(".signal-name", $dialog);
-			var $input_vendor = $(".signal-vendor", $dialog);
-			var $input_desc = $(".signal-description", $dialog);
-			
-			$dialog.dialog(
-			{
-				title: $dialog.data("title"),
-				modal: true,
-				resizable: false,
-				width: $dialog.outerWidth(true) + "px",
-				autoOpen: false
-			});
-			
-			
-			$confirm.dialog({
-				title: $confirm.data("title"),
-				modal: true,
-				resizable: false,
-				width: $confirm.outerWidth(true) + "px",
-				autoOpen: false
-			});
-			
-			$row.click(function()
-			{
-				$dialog.dialog("open");
-
-				$input_name.val(signal.name);
-				$input_vendor.val(signal.vendor);
-				$input_desc.val(signal.description);				
-			});
-			
-			$(".btn-cancel", $dialog).click(function()
-			{
-				$dialog.dialog("close");
-				return false;
-				
-			});
-			
-			$(".btn-save", $dialog).click($.proxy(function()
-			{
-				var request = $.ajax({
-					url: "scheduler/save_signal", // TODO: Change this into new signal wrapper URL.
-					type: "POST",
-					dataType: "json",
-					data: $.toJSON({ // TODO: Do we need explicit JSON conversion?
-						id: signal.id,
-						device_id: signal.device_id,
-						name: $input_name.val(),
-						vendor: $input_vendor.val(),
-						description: $input_desc.val(),
-						events: signal.events
-					})
-				});
-				
-				request.fail(function(response)
-				{
-					HC.error("<strong>Error while saving signal</strong>: " + 
-						     response.statusText + " (Error " + response.status + ")");
-				});
-				
-				request.done($.proxy(function(events)
-				{
-					HC.success("Signal \"" + $input_name.val() + "\" successfully saved.");
-					$dialog.dialog("close");
-					this.load_signals();
-					
-				}, this));
-				
-				return false;
-				
-			}, this));
-			
-			$(".btn-delete", $dialog).click(function()
-			{
-				$(".signal-name", $confirm).html(signal.name);
-				$confirm.dialog("open");
-				return false;
-			});
-
-			$(".btn-ok", $confirm).click($.proxy(function()
-			{
-				var request = $.ajax({
-					url: "signal/" + signal.id + "/sql_delete",
-					type: "GET",
-					dataType: "json"
-				});
-				
-				request.fail(function(response)
-				{
-					HC.error("<strong>Error while deleting signal \"" + signal.name + "\"</strong>: " + 
-						     response.statusText + " (Error " + response.status + ")");
-				});
-				
-				request.done($.proxy(function(events)
-				{
-					HC.success("Signal \"<strong>" + signal.name + "</strong>\" successfully deleted.");
-					$dialog.dialog("close");
-					this.load_signals();
-					
-				}, this));
-				
-				$confirm.dialog("close");
-				return false;
-				
-			}, this));
-				
-			$(".btn-cancel", $confirm).click(function()
-			{				
-				$confirm.dialog("close");
-				return false;
-			});
 		}
 	};
 	
---
+++ src/homecontrol/plugins/signal/assets/js/signal.js
@@ -0,0 +1,77 @@
+(function($)
+{
+	$.extend(HC.Signal,
+	{
+		init: function()
+		{
+			var token = document.location.href.match(/signal_id=([^&]+)/);
+			if(token.length == 0)
+				return;
+			
+			this.load_by_id(token[1], $.proxy(this.set_handlers, this));			
+			
+			return this;			
+		},
+		
+		set_handlers: function()
+		{
+			var $confirm = $(".templates .dialog-delete-signal-confirm");
+			var $input_name = $(".signal-name");
+			var $input_vendor = $(".signal-vendor");
+			var $input_desc = $(".signal-description");			
+			
+			$confirm.dialog({
+				title: $confirm.data("title"),
+				modal: true,
+				resizable: false,
+				width: $confirm.outerWidth(true) + "px",
+				autoOpen: false
+			});
+			
+			$(".btn-save").click($.proxy(function()
+			{
+				this.name = $input_name.val();
+				this.vendor = $input_vendor.val();
+				this.description = $input_desc.val();
+				this.save();
+				
+				return false;
+				
+			}, this));
+			
+			$(".btn-delete").click($.proxy(function()
+			{
+				$(".signal-name", $confirm).html(this.name);
+				$confirm.dialog("open");
+				return false;
+				
+			}, this));
+
+			$(".btn-ok", $confirm).click($.proxy(function()
+			{
+				this.delete(function()
+				{
+						if(document.referrer != "") document.location.href = document.referrer;
+						else document.location.href = "/";
+						return false;
+				});
+				
+				$confirm.dialog("close");
+				return false;
+				
+			}, this));
+				
+			$(".btn-cancel", $confirm).click(function()
+			{				
+				$confirm.dialog("close");
+				return false;
+			});
+		}		
+	});
+	
+	$(document).ready(function()
+	{
+		window.signal = Object.create(HC.Signal);
+		window.signal.init();
+	});
+})(jQuery);
