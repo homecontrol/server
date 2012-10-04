22f4ad452df54b43ad5ad9baa8d90fcf651cd2b9
Daniel Morlock <daniel.morlock@gmail.com>
2012-10-04 22:56:29 +0200

    Finished core signal implementation.

--- src/homecontrol/plugins/bootstrap/assets/js/signal.js
+++ src/homecontrol/plugins/bootstrap/assets/js/signal.js
@@ -1,32 +1,55 @@
 (function($)
 {
-	HC.Signal = 
+	HC.Signal =
 	{
 		id: null,
+		device_id: null,
 		name: null,
+		vendor: null,
 		description: null,
 		events: new Array(),
-
+		event_types: new Array(),
+		
 		load: function(data)
 		{
-			if(typeof(data) == "string")
-				data = $.evalJSON(data);
-
-			if(typeof(data) != typeof({}))
-				return Object.create(HC.Signal);
-			
-			var signal = Object.create(HC.Signal);
-			
-			signal.id = data.id;
-			signal.name = data.name;
-			signal.description = data.description;
-			
-			$.each(data.events, function(i, event_data)
-			{ signal.events.push(HC.Event.loader(event_data)) });
-			
-			return signal;
+			this.id = data["id"];
+			this.device_id = data["device_id"];
+			this.name = data["name"];
+			this.vendor = data["vendor"];
+			this.description = data["description"];
+			this.events = data["events"];
+			this.event_types = data["event_types"];
+			return this;
 		},
-
+		
+		load_by_id: function(id, callback)
+		{
+			var request  = $.ajax({
+				url: "/signal/sql_load?signal_id=" + id,
+				type: "GET",
+				dataType: "json"
+			});
+			
+			request.fail($.proxy(function(response)
+			{
+				HC.error("<strong>Could not load signal \"" + this.id + "\"</strong>: " +
+					response.statusText + " (Error " + response.status + ")");
+				
+				if(callback != undefined)
+					callback(null, response);
+				
+			}, this));
+			
+			request.done($.proxy(function(data)
+			{
+				this.load(data);
+				
+				if(callback != undefined)
+					callback(data);
+				
+			}, this));	
+		},
+		
 		json: function()
 		{
 			// Kind of workaround to support inherit properties!
@@ -34,6 +57,73 @@
 			for(p in this){ o[p] = this[p]; }
 
 			return $.toJSON(o).replace(/(:|,)/g, "$1 ");
+		},
+		
+		delete: function(callback)
+		{
+			var request = $.ajax({
+				url: "/signal/" + this.id + "/sql_delete",
+				type: "GET",
+				dataType: "json"
+			});
+			
+			request.fail(function(response)
+			{
+				HC.error("<strong>Error while deleting signal \"" + this.name + "\"</strong>: " + 
+					     response.statusText + " (Error " + response.status + ")");
+				
+				if(callback != undefined)
+					callback(false, response);
+			});
+			
+			request.done($.proxy(function()
+			{
+				HC.success("Signal \"<strong>" + this.name + "</strong>\" deleted.", function()
+				{
+					if(document.referrer != "") document.location.href = document.referrer;
+					else document.location.href = "/";
+					return false;
+				});
+				
+				if(callback != undefined)
+					callback(true, null);
+				
+			}, this));
+		},
+		
+		save: function(callback)
+		{
+			var request = $.ajax({
+				url: "/scheduler/save_signal",
+				type: "POST",
+				dataType: "json",
+				data: $.toJSON({ // TODO: Do we need explicit JSON conversion?
+					id: this.id,
+					device_id: this.device_id,
+					name: this.name,
+					vendor: this.vendor, 
+					description: this.description,
+					events: this.events
+				})
+			});
+			
+			request.fail(function(response)
+			{
+				HC.error("<strong>Error while saving signal</strong>: " + 
+					response.statusText + " (Error " + response.status + ")");
+				
+				if(callback != undefined)
+					callback(false, response);				
+			});
+			
+			request.done($.proxy(function()
+			{ 
+				HC.success("Signal \"" + this.name + "\" successfully saved.");
+				
+				if(callback != undefined)
+					callback(true, null);				
+			}, this));
 		}
-	};
+	}
 })( jQuery );
+	
